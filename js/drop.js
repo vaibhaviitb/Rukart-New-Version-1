4e5264b16f6defa9b5d0f34d4aebfd141e5fbaa2
pd1116 <70820225+pd1116@users.noreply.github.com>
2021-06-24 21:36:49 +0530

    done

---
+++ js/drop.js
@@ -0,0 +1,11 @@
+(function ($) {
+  $(".button-collapse").sideNav();
+})(jQuery);
+
+$(".dropdown-button").dropdown({
+  inDuration: 300,
+  outDuration: 225,
+  hover: true,
+  belowOrigin: true,
+  alignment: "right",
+});
